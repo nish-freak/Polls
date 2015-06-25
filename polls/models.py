b6eaf642d5879a2408be830acf525b8b1d6d4459
nish-freak <nish2493@gmail.com>
2015-06-25 16:42:47 +0530

    Define str returns in the models, add a question and its choices

--- polls/models.py
+++ polls/models.py
@@ -1,12 +1,21 @@
-from django.db import models
+import datetime
 
+from django.db import models
+from django.utils import timezone
 # Create your models here.
 
 class Question(models.Model):
 	question_test = models.CharField(max_length=200)
 	pub_date = models.DateTimeField('date published')
+	def __str__(self):
+		return unicode(self.question_test).encode('utf-8')
+
+	def was_published_recently(self):
+		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
 	question = models.ForeignKey(Question)
 	choice_text = models.CharField(max_length=200)
 	votes = models.IntegerField(default=0)
+	def __str__(self):
+		return unicode(self.choice_text).encode('utf-8')
