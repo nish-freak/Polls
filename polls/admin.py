95803371f75b5c74b877b845403895915875a17c
nish-freak <nish2493@gmail.com>
2015-06-25 20:27:09 +0530

    Added basic admin features

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    #fields = ['pub_date','question_test']
+    fieldsets = [
+        (None,              {'fields':['question_test']}),
+        ('Date information',{'fields':['pub_date'], 'classes':['collapse']}),
+    ]
+    inlines = [ChoiceInLine]
+    list_display = ['question_test','pub_date','was_published_recently']
+    list_filter = ['pub_date']
+    search_fields = ['question_test']
+
+admin.site.register(Question, QuestionAdmin)
