a5f4d53e6c833d61bf5347edc22c608f0f068eed
nish-freak <nish2493@gmail.com>
2015-07-03 16:29:43 +0530

    Improvised views functionally

--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+from django.http import Http404
+
+from .models import Question
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world! This is polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = RequestContext(request, {
+        'latest_question_list': latest_question_list,
+    })
+    return HttpResponse(template.render(context))
 
 def detail(request, question_id):
-    return HttpResponse("You are looking at the results of question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    #return HttpResponse("You are looking at the results of question %s." % question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You are looking at the results of question %s."
